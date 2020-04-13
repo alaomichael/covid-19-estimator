9f6ae188a79a9f792f5df01bd82d40f23157a3d5
alaomichael <contactleomax@gmail.com>
2020-04-14 00:46:27 +0100

    Adjusted the code for challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,24 +1,50 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const {
+    region: { avgDailyIncomeInUsd },
+    reportedCases,
+    timeToElapse,
+    periodType,
+    population,
+    totalHospitalBeds
+  } = data;
+};
 
-let impact = {
+const impact = {
   currentlyInfected: 0,
   infectionsByRequestedTime: 0
 };
 
-let severeImpact = {
-    currentlyInfected: 0,
-    infectionsByRequestedTime: 0
+const severeImpact = {
+  currentlyInfected: 0,
+  infectionsByRequestedTime: 0
 };
 
-// To estimate the number of currently infected
-impact.currentlyInfected = covid19ImpactEstimator.reportedCases * 10;
+// To estimate the number of currently infected and taking off the decimal
+impact.currentlyInfected = Math.trunc(reportedCases * 10);
+
+// To estimate the severe impact and taking off the decimal
+severeImpact.currentlyInfected = Math.trunc(reportedCases * 50);
+
+// Check if the timeToElapse is in days weeks or month
+let timeFactor;
+switch (periodType.trim().toLowerCase()) {
+  case 'months':
+    timeFactor = Math.trunc((timeToElapse * 30) / 3);
+    break;
+  case 'weeks':
+    timeFactor = Math.trunc((timeToElapse * 7) / 3);
+    break;
+  case 'days':
+    timeFactor = Math.trunc((timeToElapse) / 3);
+    break;
+  default:
+};
 
 // To estimate the number of infected people 28 days from now, as currently infected people doubles every 3 days i.e 2 to the power of 9
-impact.infectionsByRequestedTime = impact.currentlyInfected * 512;
+impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** timeFactor);
 
-// To estimate the severe impact
-severeImpact.currentlyInfected = covid19ImpactEstimator.reportedCases * 50;
 
 // To estimate the number of infected people 28 days from now, as currently infected people doubles every 3 days i.e 2 to the power of 9
-severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 512;
+severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** timeFactor);
+
 export default covid19ImpactEstimator;
